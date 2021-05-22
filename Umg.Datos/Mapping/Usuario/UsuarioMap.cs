9cce7377f669898ed8cce0ff0c1bbef5eb011b91
1000tom <mmoralesl19@miumg.edu.gt>
2021-05-22 12:17:47 -0600

    umg sistemas

---
+++ Umg.Datos/Mapping/Usuario/UsuarioMap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Umg.Entidades.Usuario;
+
+namespace Umg.Datos.Mapping.Usuario
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder) 
+        {
+            builder.ToTable("usuario")
+              .HasKey(c => c.idUsuario);
+            builder.Property(c => PrimerNombre )
+                .HasMaxLength(20);
+            builder.Property(c => SegundoNombre)
+                .HasMaxLength(20);
+            builder.Property(c => PrimerApellido)
+                .HasMaxLength(20);
+            builder.Property(c => SegundoApellido)
+                .HasMaxLength(20);
+            builder.Property(c => Direccion)
+                .HasMaxLength(20);
+            builder.Property(c => Telefono)
+                .HasMaxLength(20);
+            builder.Property(c => email)
+                .HasMaxLength(20);
+            builder.Property(c => passwordHash)
+                .HasMaxLength(20);
+            builder.Property(c => passwordSal)
+                .HasMaxLength(20);
+            builder.Property(c => condicion)
+                .HasMaxLength(20);
+
+        }
+
+            
+        }
+    
+    }
+
---
+++ Umg.Datos/Mapping/Ventas/VentaMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Umg.Entidades.Ventas;
+
+namespace Umg.Datos.Mapping.Ventas
+{
+    public class VentaMap : IEntityTypeConfiguration<Venta>
+    {
+        public void Configure(EntityTypeBuilder<Venta> builder)
+        {
+
+        
+            
+        }
+    }
+}
